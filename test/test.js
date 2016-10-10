e3a6663a36ed089d808997763cca681bd06e299d
daniel-ece <minhdaniel.95@gmail.com>
2016-10-10 10:06:40 +0200

    add test

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var should = require('should');
+var user = require('../src/user.js');
+
+
+describe('my first text list', function(){
+	it('should get a user with right parameters', function(done) {
+			user.get("Daniel", function(res) {
+			res.should.equal("Daniel");
+			done();
+		})
+	})
+});
